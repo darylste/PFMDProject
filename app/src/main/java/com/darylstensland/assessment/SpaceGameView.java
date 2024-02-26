3954ee2b2d3dcd431be318c088eaa39bb99b98b2
Daryl Stensland <dstensland5@gmail.com>
2024-02-26 10:48:20 +0000

    Barebones code from Aula Week 7 added

--- app/src/main/java/com/darylstensland/assessment/SpaceGameView.java
+++ app/src/main/java/com/darylstensland/assessment/SpaceGameView.java
@@ -13,9 +13,11 @@ import android.view.SurfaceView;
 
 import java.io.IOException;
 
-public class SpaceGameView extends SurfaceView implements Runnable{
+public class SpaceGameView extends SurfaceView implements Runnable {
 
     private Context context;
+
+    private Spaceship spaceship;
 
     private Thread gameThread = null;
     private SurfaceHolder ourHolder;
@@ -38,14 +40,11 @@ public class SpaceGameView extends SurfaceView implements Runnable{
     private int lives = 3;
 
 
-
     public SpaceGameView(Context context, int x, int y) {
-
         super(context);
 
 
         this.context = context;
-
 
         ourHolder = getHolder();
         paint = new Paint();
@@ -53,15 +52,13 @@ public class SpaceGameView extends SurfaceView implements Runnable{
         screenX = x;
         screenY = y;
 
-
+        spaceship = new Spaceship(context, x, y);
 
         initLevel();
     }
 
 
-
-    private void initLevel(){
-
+    private void initLevel() {
 
 
     }
@@ -75,7 +72,7 @@ public class SpaceGameView extends SurfaceView implements Runnable{
             long startTime = System.currentTimeMillis();
 
 
-            if(!paused){
+            if (!paused) {
                 update();
             }
 
@@ -93,35 +90,27 @@ public class SpaceGameView extends SurfaceView implements Runnable{
     }
 
 
-
-    private void update(){
-
+    private void update() {
 
 
     }
 
 
-
-
-
-
-    private void draw(){
+    private void draw() {
 
         if (ourHolder.getSurface().isValid()) {
 
             canvas = ourHolder.lockCanvas();
+            canvas.drawBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.background), 0, 0, paint);
+            canvas.drawBitmap(Spaceship.getBitmap(), Spaceship.getX(), Spaceship.getY(), paint);
+            paint.setColor(Color.argb(255, 249, 129, 0));
 
-
-            canvas.drawColor(Color.argb(255, 26, 128, 182));
-
-            paint.setColor(Color.argb(255,  249, 129, 0));
             paint.setTextSize(40);
             canvas.drawText("Score: " + score + "   Lives: " +
-                    lives, 10,50, paint);
+                    lives, 10, 50, paint);
             ourHolder.unlockCanvasAndPost(canvas);
         }
     }
-
 
 
     public void pause() {
@@ -140,6 +129,55 @@ public class SpaceGameView extends SurfaceView implements Runnable{
         gameThread.start();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+
+            // Player has touched the screen
+
+            case MotionEvent.ACTION_DOWN:
+
+                paused = false;
+
+                if(motionEvent.getY() > screenY - screenY / 2) {
+                    if (motionEvent.getX() > screenX / 2) {
+                        spaceship.setMovementState(spaceship.RIGHT);
+                    } else {
+                        spaceship.setMovementState(spaceship.LEFT);
+                    }
 
 
-}  // end class
+                }
+
+                if(motionEvent.getY() < screenY - screenY / 2) {
+                    if (motionEvent.getX() > screenX / 2) {
+                        spaceship.setMovementState(spaceship.UP);
+                    } else {
+                        spaceship.setMovementState(spaceship.DOWN);
+                    }
+
+
+                }
+
+                //    if(motionEvent.getY() < screenY - screenY / 8) {
+                // Shots fired
+                //       if(bullet.shoot(playerShip.getX()+ playerShip.getLength()/2,screenY,bullet.UP)){
+                //        soundPool.play(shootID, 1, 1, 0, 0, 1);
+                //       }
+                //   }
+
+                break;
+
+            // Player has removed finger from screen
+            case MotionEvent.ACTION_UP:
+
+                //   if(motionEvent.getY() > screenY - screenY / 10) {
+                spaceship.setMovementState(spaceship.STOPPED);
+                //   }
+                break;
+        }
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/darylstensland/assessment/Spaceship.java
@@ -0,0 +1,124 @@
+package com.darylstensland.assessment;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.RectF;
+
+public class Spaceship {
+
+    RectF rect;
+    private static Bitmap bitmap;
+    private Bitmap bitmapleft;
+    private Bitmap bitmapright;
+    private Bitmap bitmapdown;
+    public Bitmap currentBitmap;
+    private float height;
+    private float length;
+    private static float x;
+    private static float y;
+
+    private float SpaceShipSpeed;
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+    public final int UP = 3;
+    public final int DOWN = 4;
+
+    ///maybe more movement than this
+    private int SpaceShipMoving = STOPPED;
+    private int shipSpeed;
+
+    public Spaceship(Context context, int screenX, int screenY){
+
+        rect = new RectF();
+
+        length = screenX/10;
+        height = screenY/10;
+
+        x = screenX / 2;
+        y = screenY / 2;
+
+        shipSpeed = 350;
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceship_right);
+
+        // stretch the bitmap to a size appropriate for the screen resolution
+        bitmap = Bitmap.createScaledBitmap(bitmap,
+                (int) (length),
+                (int) (height),
+                false);
+
+        //  bitmapup = BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceshipup);
+        //  bitmapup = Bitmap.createScaledBitmap(bitmapup, (int) (length), (int) (height),false);
+
+        bitmapright = BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceship_right);
+        bitmapright = Bitmap.createScaledBitmap(bitmapright, (int) (length), (int) (height),false);
+
+        bitmapleft = BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceship_left);
+        bitmapleft = Bitmap.createScaledBitmap(bitmapleft, (int) (length), (int) (height),false);
+
+        bitmapdown = BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceship_down);
+        bitmapdown = Bitmap.createScaledBitmap(bitmapdown, (int) (length), (int) (height),false);
+
+         currentBitmap = bitmap;
+    }
+
+    public static float getY() {
+        return y;
+    }
+
+    public void setMovementState(int state){
+        SpaceShipMoving = state;
+    }
+
+
+    public void update(long fps){
+          if(SpaceShipMoving == LEFT){
+              x = x - SpaceShipSpeed / fps;
+               currentBitmap = bitmapleft;
+           }
+            if(SpaceShipMoving == RIGHT){
+                x = x + SpaceShipSpeed / fps;
+                currentBitmap = bitmapright;
+            }
+            if(SpaceShipMoving == UP){
+                y = y - SpaceShipSpeed / fps;
+         currentBitmap = bitmap;
+
+            }
+
+            if(SpaceShipMoving == DOWN){
+                y=y + SpaceShipSpeed / fps;
+           currentBitmap = bitmapdown;
+
+           }
+
+        rect.top = y;
+        rect.bottom = y + height;
+        rect.left = x;
+        rect.right = x + length;
+
+    }
+
+
+    public RectF getRect(){
+        return rect;
+    }
+
+    public static Bitmap getBitmap(){
+
+        return bitmap;
+    }
+
+    public static float getX(){
+        return x;
+    }
+
+    public float getLength(){
+        return length;
+    }
+
+
+
+
+}
